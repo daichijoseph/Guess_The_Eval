cc93339086cdd528efe57e12c1c2200a510e2f8a
Daichi Watanabe <daichijoseph@gmail.com>
2025-01-13 14:15:29 +0900

    first commit

---
+++ prob_to_centipawn.py
@@ -0,0 +1,7 @@
+from math import log, exp
+
+def prob_to_centipawns(w):
+    return -log((1-w)/w)/0.00368208
+
+def centipawn_to_prob(cp):
+    return (50 + 50*(2/(1 + exp(-0.00368208*cp)) - 1))/100
