3363c014c6b4bdefbffdaeb721bead8f2b0ff39f
Daichi Watanabe <daichijoseph@gmail.com>
2025-01-15 14:40:17 +0900

    added function to fetch games by player, rating range

--- LichessData.py
+++ LichessData.py
@@ -1,4 +1,6 @@
 import berserk
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 from config import API_TOKEN
 # berserk is a Python package designed to be used with Lichess's API
 
@@ -6,4 +8,32 @@ from config import API_TOKEN
 def create_client():
     session = berserk.TokenSession(API_TOKEN)
     return berserk.client(session=session)
+    
+def get_games(
+        client,
+        from_rating=1300,
+        to_rating=1400,
+        player='daichijoseph41664',
+        rated=True,
+        time_control='rapid',
+        num_games=50
+):
+    """Fetch games by player from the last month"""
 
+    end = datetime.now()
+    start = end - relativedelta(months=1)
+    games = client.games.export_by_player(
+        username=player,
+        since=berserk.utils.to_millis(start),
+        until=berserk.utils.to_millis(end),
+        max=num_games,
+        perf_type=time_control
+    )
+
+    filtered_games = []
+    for game in games:
+        white_rating = game['players']['white'].get('rating', 0)
+        black_rating = game['players']['black'].get('rating', 0)
+        if (from_rating <= white_rating <= to_rating and from_rating <= black_rating <= to_rating):
+            filtered_games.append(game)
+    return filtered_games
